e62e7406e0d5d11fba97c221e7b1c577c267d57e
ELIAS-1829 <121119836+ELIAS-1829@users.noreply.github.com>
2022-12-24 04:24:59 -0300

    Add files via upload

---
+++ instalacion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#CURSO DE PROGRAMACION
+cr="\033[38;5;31m"
+ro="\033[38;1;31m"
+ce="\033[38;5;31m"
+echo -e "$ce *************************"
+echo -e "$cr   bienvenido al banner "
+echo -e "$ce ************************"
+echo 
+echo
+read -p "aprete enter para iniciar el banner " banner
+
+bash banner.sh 
+
+echo -e "$ro HABRA OTRA SECION PARA INICIAR EL BANNER "
+
+echo""
+echo -e "$cr gracias por usar mi banner  "
