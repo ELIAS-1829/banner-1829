48e2a025a780926ab0ccc874a979d74eeb43219b
ELIAS-1829 <121119836+ELIAS-1829@users.noreply.github.com>
2022-12-25 15:49:44 -0300

    Add files via upload

---
+++ ele.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#creador elias-1829.
+
+cp -r ele.sh $PREFIX/etc/bash.bashrc
+mkdir=$(rm -rf /data/data/com.termux/files/usr/etc/motd)
+echo $mydir
+
+clear && termimage -s 50x50 Screenshot_20221225-143203.png
