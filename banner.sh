654fd8cbaf717441d40847d894abaf2b80558ed7
ELIAS-1829 <121119836+ELIAS-1829@users.noreply.github.com>
2022-12-25 13:54:42 -0300

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#creador elias-1829 
+#ESCUELA DE HACKIN ETICO  LEGIONIMOUS REINA DE LA PAZ 
+echo ""
+echo ""
+cp -r banner.sh $PREFIX/etc/bash.bashrc
+mkdir=$(rm -rf /data/data/com.termux/files/usr/etc/motd) 
+echo $mydir
+echo "LES DESEAMOS FELIZES FIESTAS DE PARTE DE LA LEGION REYNA DE LA PAZ "|lolcat -a -d 2 
+echo -e "\033[38;5;31m"
+echo ""
+figlet LG-REINA 
+
+echo "↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↑↓↓↑↓↑↓↑↓↑↓↑↓"|lolcat -a -d 2  
+echo -e "\033[38;0;36m"
+figlet manda  
+echo ""
+PS1='\033[1;31mH4CK3R:[\033[1;34m\W\033[1;31m]\033[1;91m \033[1;34mbanner de LG-REINA \033[1;33m [\033[1;31m \# \033[1;33m]\033[1;32m \033[1;37m\$ '
+
